4ca94686ef2e97cfe1bf77966d8bc03656403645
tuuzdu <tuuzdu@mail.ru>
2018-07-21 17:07:59 +0300

    WaspmoteIPFS tested

--- de_airsense/src/de_airsense_waspmote_ipfs.py
+++ de_airsense/src/de_airsense_waspmote_ipfs.py
@@ -9,133 +9,144 @@ import ipfsapi
 from std_msgs.msg import UInt8, Float32, String
 from sensor_msgs.msg import NavSatFix
 import rospy
+from threading import Thread
 
-DEFAULT_PORT = '/dev/ttyUSB0'
-BAUDRATE = 115200
-STOP_SYMBOL = b'$'
-RATE_HZ = 1
-IN_AIR_STANDBY = 3
-ON_GROUND = 1
+class WaspmoteIPFS:
 
-status_in_air = False
-status_to_send = False
-altitude_rel = 0.0
-altitude_gps = 0.0
-latitude = 0.0
-longitude = 0.0
+    status_in_air = False
+    status_to_send = False
+    altitude_rel = 0.0
+    altitude_gps = 0.0
+    latitude = 0.0
+    longitude = 0.0
 
-def ipfs_send (api, file):
-    try:
-        res = api.add(file)
-        return res
-    except:
-        return 'IPFS error'
+    def __init__(self):
+        DEFAULT_PORT = '/dev/ttyUSB0'
+        BAUDRATE = 115200
+        IN_AIR_STANDBY = 3
+        ON_GROUND = 1
 
-def write_send_data(frame_array, api_loc):
-    fileName = 'data_'
-    dir = os.path.dirname(__file__)
-    folderName = os.path.join(dir, 'sensor_data/')
-    if not os.path.exists(os.path.dirname(folderName)):
-        try:
-            os.makedirs(os.path.dirname(folderName))
-        except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
-                
-    timestr = time.strftime('%Y-%m-%d_%H-%M-%S')
-    path = folderName + fileName + timestr + '.txt'
-    with open (path, 'w') as f:
-        for string in frame_array:
-            f.write(string)
-    rospy.loginfo('File wrote in')
-    rospy.loginfo(path)
+        rospy.loginfo('Starting waspmote gas sensors...')
+        rospy.loginfo('Waiting for ROS services...')
+        rospy.init_node('de_airsense_waspmore_ipfs')
 
-    # rospy.loginfo('IPFS remote node response:')
-    # res = ipfs_send(api_rem, path)
-    # rospy.loginfo(res)
-    rospy.loginfo('IPFS local node response:')
-    res = ipfs_send(api_loc, path)
-    rospy.loginfo(res)
-    result = String()
-    result.data = res['Hash']
-    result_pub(result)
-    
-def flight_status_cb(data): 
-    global status_in_air
-    global status_to_send
-    if data.data == IN_AIR_STANDBY and status_in_air == False:
-        status_in_air = True
-        rospy.loginfo('Start to write data')
-    elif data.data == ON_GROUND and status_in_air == True:
-        rospy.loginfo('Stop to write data')
-        status_to_send = True
-        status_in_air = False
+        def flight_status(data_msg): 
+            if data_msg.data == IN_AIR_STANDBY and self.status_in_air == False:
+                self.status_in_air = True
+                rospy.loginfo('Start to write data')
+            elif data_msg.data == ON_GROUND and self.status_in_air == True:
+                rospy.loginfo('Stop to write data')
+                self.status_to_send = True
+                self.status_in_air = False
+        rospy.Subscriber('dji_sdk/flight_status', UInt8, flight_status)
 
-def gps_position_cb(data):
-    global latitude
-    global longitude
-    global altitude_gps
-    latitude = data.latitude
-    longitude = data.longitude
-    altitude_gps = data.altitude
+        def gps_position(data_msg):
+            self.latitude = data_msg.latitude
+            self.longitude = data_msg.longitude
+            self.altitude_gps = data_msg.altitude
+        rospy.Subscriber('dji_sdk/gps_position', NavSatFix, gps_position)
 
-def height_above_takeoff_cb(data):
-    global altitude_rel
-    altitude_rel = data.data
+        def height_above_takeoff(data_msg):
+            self.altitude_rel = data_msg.data
+        rospy.Subscriber('dji_sdk/height_above_takeoff', Float32, height_above_takeoff)
+
+        self.result_pub = rospy.Publisher('~result/measurements', String, queue_size=10)
+        self.ipfs_api_loc = ipfsapi.connect('127.0.0.1', 5001)
+        # ipfs_api_rem = ipfsapi.connect('52.178.98.62', 9095)
+
+        self.serial_port = serial.Serial(   DEFAULT_PORT, 
+                                            BAUDRATE, 
+                                            parity=serial.PARITY_NONE, 
+                                            stopbits=serial.STOPBITS_ONE, 
+                                            bytesize=serial.EIGHTBITS)
+
+        thread = Thread(target=self.serial_receiver)
+        thread.daemon = True
+        thread.start()
+
+    def serial_receiver(self):
+        STOP_SYMBOL = b'$'
+        rate = rospy.Rate(1)
+        frame = ''
+        frame_array = []
+        waspmote_ready = False
+        while not rospy.is_shutdown():
+            try:    
+                while self.serial_port.in_waiting > 0:
+                    if waspmote_ready == False:
+                        waspmote_ready = True
+                        rospy.loginfo('Waspmote gas sensors and ROS are ready')
+                    byte = self.serial_port.read()
+
+                    if byte == STOP_SYMBOL:
+                        # frame += byte.decode()  
+                        frame +=    'Copter Latitude: {0:.6f}\n' \
+                                    'Copter Longitude: {1:.6f}\n' \
+                                    'Copter GPS Altitude: {2:.2f} m\n' \
+                                    'Copter Relative Altitude: {3:.2f} m\n' \
+                                    'System time: {4:s}\n'.format(  self.latitude, 
+                                                                    self.longitude, 
+                                                                    self.altitude_gps,
+                                                                    self.altitude_rel, 
+                                                                    time.strftime('%Y/%m/%d %H:%M:%S'))
+                        frame_array.append(frame)
+                        rospy.loginfo(frame)
+                        frame = ''
+                        continue
+                    elif byte != b'\x86' and byte != b'\x00':
+                        frame += byte.decode()
+
+                if self.status_to_send:
+                    self.status_to_send = False
+                    self.write_send_data(frame_array)
+                    frame_array = []
+                elif self.status_in_air == False:
+                    frame_array = []
+
+                rate.sleep()
+
+            except KeyboardInterrupt: 
+                rospy.loginfo('\nExit')
+                break
+
+    def write_send_data(self, frame_array):
+        fileName = 'data_'
+        dir = os.path.dirname(__file__)
+        folderName = os.path.join(dir, 'sensor_data/')
+        if not os.path.exists(os.path.dirname(folderName)):
+            try:
+                os.makedirs(os.path.dirname(folderName))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+                    
+        timestr = time.strftime('%Y-%m-%d_%H-%M-%S')
+        path = folderName + fileName + timestr + '.txt'
+        with open (path, 'w') as f:
+            for string in frame_array:
+                f.write(string)
+        rospy.loginfo('File wrote in')
+        rospy.loginfo(path)
+
+        def ipfs_send (api, file):
+            try:
+                res = api.add(file)
+                return res
+            except:
+                return 'IPFS error'
+
+        res = ipfs_send(self.ipfs_api_loc, path)
+        rospy.logdebug('IPFS local node response:')
+        rospy.logdebug(res)
+        if res['Hash']:
+            rospy.loginfo('IPFS file hash:')
+            rospy.loginfo(res['Hash'])
+            result_msg = String()
+            result_msg.data = res['Hash']
+            self.result_pub.publish(result_msg)
+
+    def spin(self):
+        rospy.spin()
 
 if __name__ == '__main__':
-    rospy.loginfo('Starting waspmote gas sensors...')
-    rospy.loginfo('Waiting for ROS services...')
-    rospy.init_node('de_airsense_waspmore_ipfs')
-    rospy.Subscriber('dji_sdk/flight_status', UInt8, flight_status_cb)
-    rospy.Subscriber('dji_sdk/gps_position', NavSatFix, gps_position_cb)
-    rospy.Subscriber('dji_sdk/height_above_takeoff', Float32, height_above_takeoff_cb)
-    result_pub = rospy.Publisher('~result/measurements', String, queue_size=10)
-    ipfs_api_loc = ipfsapi.connect('127.0.0.1', 5001)
-    # ipfs_api_rem = ipfsapi.connect('52.178.98.62', 9095)
-
-    rate = rospy.Rate(RATE_HZ)
-    frame = ''
-    frame_array = []
-    serial_port = serial.Serial(DEFAULT_PORT, BAUDRATE, parity=serial.PARITY_NONE, 
-                                                        stopbits=serial.STOPBITS_ONE, 
-                                                        bytesize=serial.EIGHTBITS)
-    waspmote_ready = False
-    while not rospy.is_shutdown():
-        try:    
-            while serial_port.in_waiting > 0:
-                if waspmote_ready == False:
-                    waspmote_ready = True
-                    rospy.loginfo('Waspmote gas sensors and ROS are ready')
-                byte = serial_port.read()
-
-                if byte == STOP_SYMBOL:
-                    # frame += byte.decode()  
-                    frame += '''Copter Latitude: {0:.6f}\n
-                                Copter Longitude: {1:.6f}\n
-                                Copter GPS Altitude: {2:.2f} m\n
-                                Copter Relative Altitude: {3:.2f} m\n
-                                System time: {4:s}\n'''.format( latitude, 
-                                                                longitude, 
-                                                                altitude_gps,
-                                                                altitude_rel, 
-                                                                time.strftime('%Y/%m/%d %H:%M:%S'))
-                    frame_array.append(frame)
-                    rospy.loginfo(frame)
-                    frame = ''
-                    continue
-                elif byte != b'\x86' and byte != b'\x00':
-                    frame += byte.decode()
-
-            if status_to_send:
-                status_to_send = False
-                write_send_data(frame_array, ipfs_api_loc)
-                frame_array = []
-            elif status_in_air == False:
-                frame_array = []
-
-            rate.sleep()
-
-        except KeyboardInterrupt: 
-            rospy.loginfo('\nExit')
-            break
+    WaspmoteIPFS().spin()
