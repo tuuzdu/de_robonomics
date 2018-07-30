a5f4b999ee7015a00c9675c1d310d602a3bb2f9d
tuuzdu <tuuzdu@mail.ru>
2018-07-30 17:22:51 +0300

    Fix bugs

--- de_airsense/src/de_airsense_agent.py
+++ de_airsense/src/de_airsense_agent.py
@@ -11,8 +11,8 @@ from threading import Thread
 
 class Agent:
 
-	current_measurement = None
-	# in_work = False
+    current_measurement = None
+    # in_work = False
 
     def __init__(self):
         rospy.init_node('de_airsense_agent')
@@ -21,21 +21,22 @@ class Agent:
         self.token = rospy.get_param('~token')
         self.bid_lifetime = rospy.get_param('~bid_lifetime')
         self.web3 = Web3(HTTPProvider(rospy.get_param('~web3_http_provider')))
-        self.signing_bid_pub = rospy.Publisher('lighthouse/infochan/signing/bid', Bid, queue_size=10)
+        self.signing_bid_pub = rospy.Publisher('liability/infochan/signing/bid', Bid, queue_size=10)
 
         def incoming_ask(ask_msg):
+            rospy.loginfo('Incoming ask: ' + str(ask_msg))
             if ask_msg.model == self.model and ask_msg.token == self.token:
             	# self.in_work = True
                 rospy.loginfo('Incoming ask with right model and token.')
                 self.make_bid(ask_msg)
             else:
                 rospy.loginfo('Incoming ask with wrong model and token, skip.')
-        rospy.Subscriber('lighthouse/infochan/incoming/ask', Ask, incoming_ask)
+        rospy.Subscriber('liability/infochan/incoming/ask', Ask, incoming_ask)
 
         def measurements(hash_msg):
             self.current_measurement = hash_msg.data
             rospy.loginfo('Received measurements in IPFS file: ' + hash_msg.data)
-        rospy.Subscriber('de_airsense_waspmore_ipfs/result/measurements', String, measurements)
+        rospy.Subscriber('de_airsense_waspmote_ipfs/result/measurements', String, measurements)
 
         rospy.wait_for_service('liability/finish')
         self.finish_srv = rospy.ServiceProxy('liability/finish', Empty)
@@ -52,7 +53,7 @@ class Agent:
         bid.cost = incoming_ask.cost
         bid.lighthouseFee = 0
         bid.deadline = self.web3.eth.getBlock('latest').number + self.bid_lifetime
-        self.signing_bid_pub(bid)
+        self.signing_bid_pub.publish(bid)
 
     def process(self): # to subscriber cb?
         while True:
