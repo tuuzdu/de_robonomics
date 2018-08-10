f5474161e20f4e95b4359081eb6a72d0beef739b
tuuzdu <tuuzdu@mail.ru>
2018-08-10 23:33:59 +0300

    IPFS objective check

--- de_airsense/src/de_airsense/agent.py
+++ de_airsense/src/de_airsense/agent.py
@@ -3,15 +3,15 @@
 from robonomics_lighthouse.msg import Ask, Bid
 from std_msgs.msg import String
 from std_srvs.srv import Empty
-import rospy
 from web3 import Web3, HTTPProvider
 from threading import Thread
+from urllib.parse import urlparse
+import rospy, ipfsapi
 
 
 class Agent:
 
     current_measurement = None
-    # in_work = False
 
     def __init__(self):
         rospy.init_node('de_airsense_agent')
@@ -20,16 +20,30 @@ class Agent:
         self.token = rospy.get_param('~token')
         self.bid_lifetime = rospy.get_param('~bid_lifetime')
         self.web3 = Web3(HTTPProvider(rospy.get_param('~web3_http_provider')))
+        ipfs_provider = urlparse(rospy.get_param('~ipfs_http_provider')).netloc.split(':')
+        self.ipfs = ipfsapi.connect(ipfs_provider[0], int(ipfs_provider[1]))
         self.signing_bid_pub = rospy.Publisher('liability/infochan/signing/bid', Bid, queue_size=10)
 
         def incoming_ask(ask_msg):
             rospy.loginfo('Incoming ask: ' + str(ask_msg))
             if ask_msg.model == self.model and ask_msg.token == self.token:
-            	# self.in_work = True
-                rospy.loginfo('Incoming ask with right model and token.')
+                rospy.loginfo('Incoming ask with right model and token')
+
+                def objective_check():
+                    rospy.loginfo('Waiting for objective file from IPFS...')
+                    self.ipfs.ls(ask_msg.objective)
+                    # msg_type = list(bag.get_type_and_topic_info()[0].values())[0]
+                check_thread = Thread(target=objective_check)
+                check_thread.start()
+                check_thread.join(60)
+                if check_thread.is_alive():
+                    rospy.logwarn('Timeout of waiting objective file: ' + str(ask_msg.objective))
+                    rospy.logwarn('Skip incoming ask')
+                    return
+
                 self.make_bid(ask_msg)
             else:
-                rospy.loginfo('Incoming ask with wrong model and token, skip.')
+                rospy.logwarn('Incoming ask with wrong model and token, skip')
         rospy.Subscriber('liability/infochan/incoming/ask', Ask, incoming_ask)
 
         def measurements(hash_msg):
@@ -44,7 +58,6 @@ class Agent:
 
     def make_bid(self, incoming_ask):
         rospy.loginfo('Making bid...')
-
         bid = Bid()
         bid.model = self.model
         bid.objective = incoming_ask.objective
@@ -54,13 +67,12 @@ class Agent:
         bid.deadline = self.web3.eth.getBlock('latest').number + self.bid_lifetime
         self.signing_bid_pub.publish(bid)
 
-    def process(self): # to subscriber cb?
+    def process(self):
         while True:
-            while not self.current_measurement: # and not self.in_work
+            while not self.current_measurement:
                 rospy.sleep(1)
             self.current_measurement = None
             self.finish_srv()
-            # self.in_work = False
             rospy.loginfo('Liability finished')
     
     def spin(self):
