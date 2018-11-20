8e252bc8375d5624b0da8c379c56bc59161fb9d9
tuuzdu <tuuzdu@mail.ru>
2018-11-20 16:31:11 +0300

    One package Nix build

--- default.nix
+++ default.nix
@@ -1,16 +1,26 @@
-{ lib ? import ./nix/lib.nix { }
+{ stdenv
+, ros_comm
+, mkRosPackage
+, python3Packages
+, robonomics_comm
+, dji_sdk
+, mavros
 }:
 
-let
-  callPackage = lib.nixpkgs.callPackage;
+mkRosPackage rec {
+  name = "${pname}-${version}";
+  pname = "de_robonomics";
+  version = "master";
 
-in rec {
-  de_msgs = callPackage ./nix/msgs.nix { };
-  de_airsense = callPackage ./nix/airsense.nix { 
-  	inherit de_msgs;
-  };
-  de_robonomics = callPackage ./nix/default.nix { 
-  	inherit de_msgs;
-  	inherit de_airsense;
+  src = ./.;
+
+  propagatedBuildInputs = with python3Packages;
+  [ dji_sdk mavros robonomics_comm ros_comm web3 ipfsapi pyserial ];
+
+  meta = with stdenv.lib; {
+    description = "Drone Employee and Robonomics stack";
+    homepage = http://github.com/tuuzdu/de_robonomics;
+    license = licenses.bsd3;
+    maintainers = [ maintainers.tuuzdu ];
   };
 }
